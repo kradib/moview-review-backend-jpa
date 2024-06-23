c03b7f49ab80711bbb211bf77e465dae769a77ae
kradib <radibkar32@gmail.com>
2024-06-23 22:50:38 +0530

    In memory moview review system

--- src/main/java/com/example/movie_review_system_copy/respository/MovieRepository.java
+++ src/main/java/com/example/movie_review_system_copy/respository/MovieRepository.java
@@ -4,12 +4,16 @@ package com.example.movie_review_system_copy.respository;
 import com.example.movie_review_system_copy.dto.CreateMovieRequest;
 import com.example.movie_review_system_copy.model.Movie;
 import com.example.movie_review_system_copy.model.Review;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Repository
+@Slf4j
 public class MovieRepository {
 
     private final List<Movie> movies;
@@ -37,14 +41,22 @@ public class MovieRepository {
                 .title(createMovieRequest.getTitle())
                 .trailerLink(createMovieRequest.getTrailerLink())
                 .poster(createMovieRequest.getPosterLink())
+                .reviews(new ArrayList<>())
                 .build();
         movies.add(movie);
         return movie;
     }
 
-    public int updateMovie(final int movieId, final Review review) {
-        return 1;
+    public void updateMovie(final int movieId, Review review) {
+        Movie movie = getAMovie(movieId);
+        log.info("Fetched the movie: {}", movie);
+        if(movie == null) {
+            log.error("The requested id does not exist");
+        } else {
+            List<Review> reviews = movie.getReviews();
+            reviews.add(review);
+            movie.setReviews(reviews);
+        }
     }
-
 
 }
--- src/main/java/com/example/movie_review_system_copy/respository/ReviewRepository.java
+++ src/main/java/com/example/movie_review_system_copy/respository/ReviewRepository.java
@@ -2,14 +2,16 @@ package com.example.movie_review_system_copy.respository;
 
 import com.example.movie_review_system_copy.dto.CreateReviewRequest;
 import com.example.movie_review_system_copy.model.Review;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
 
 @Repository
+@RequiredArgsConstructor
 public class ReviewRepository {
 
-    private MovieRepository movieRepository;
+    private final MovieRepository movieRepository;
 
 
     public Review createReview(final int movieId, final CreateReviewRequest createReviewRequest) {
